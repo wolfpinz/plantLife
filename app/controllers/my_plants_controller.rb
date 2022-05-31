b75936db7520f3557883217fd2a23b29349d8534
Wolf Pinz <wolfpinz@gmail.com>
2022-05-31 17:30:42 +0200

    added my_plants controller, changed garden show + my_plants new views

--- app/controllers/my_plants_controller.rb
+++ app/controllers/my_plants_controller.rb
@@ -1,8 +1,35 @@
 class MyPlantsController < ApplicationController
+  before_action :set_garden, only: [ :new, :create]
+  before_action :set_plant, only: [ :new, :create]
   skip_before_action :authenticate_user!, only: [ :new, :create ]
 
   def new
     @my_plant = MyPlant.new
+    raise
   end
 
+  def create
+    my_plant = MyPlant.new(my_plant_params)
+    my_plant.plant = @plant
+    raise
+    if my_plant.save
+      redirect_to gardens_path(@garden)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_garden
+    @garden = Garden.find(params[:garden_id])
+  end
+
+  def set_plant
+    @plant = Plant.find(params[:plant_id])
+  end
+
+  def my_plant_params
+    params.require(:my_plant).permit(:nickname, :light_exposure, :photo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :gardens, only: [:show, :new, :create, :destroy] do
+    resources :plants, only: [:index]
     resources :my_plants, only: [:show, :new, :create, :edit, :update, :destroy]
   end
 end
