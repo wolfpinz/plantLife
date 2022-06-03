01bfd0c550ae4e50796c2cce5653a762a4fd6b27
Wolf Pinz <wolfpinz@gmail.com>
2022-06-03 15:47:08 +0200

    implemented different colors vor due today and overdue in notifications

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "input" ]
 
-  connect() {
-    console.log("Hello from our first Stimulus controller")
-  }
-
   toggleStyle() {
     this.element.classList.add("opacity-100")
   }
