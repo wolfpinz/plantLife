d53472a1ac832b30a81887eb431878604f5e2cc4
danielschuetzle <danielschuetzle@gmail.com>
2022-06-06 14:21:36 +0200

    Garden tabs added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @garden_first = Garden.first
     @gardens = Garden.all
     @garden = Garden.new
     @my_plants = MyPlant.all.order(:last_watered)
