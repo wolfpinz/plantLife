2ca96e4d5f4447d172a80dd0c89a294c99cc7fb9
danielschuetzle <danielschuetzle@gmail.com>
2022-06-06 10:22:45 +0200

    garden card added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @garden_first = Garden.first
     @gardens = Garden.all
     @garden = Garden.new
     @my_plants = MyPlant.all.order(:last_watered)
