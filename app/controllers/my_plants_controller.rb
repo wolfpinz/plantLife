0df62027727f297e58e08f48aa92c8224db74f9b
vitaliy <vitaliy.skoryy@googlemail.com>
2022-06-08 16:23:56 +0200

    working controllers and models now

--- app/controllers/my_plants_controller.rb
+++ app/controllers/my_plants_controller.rb
@@ -33,6 +33,12 @@ class MyPlantsController < ApplicationController
     end
   end
 
+  def index
+    # display plants the user searched for
+    @garden = Garden.find(params[:garden_id])
+    @my_plants = @garden.my_plants
+  end
+
   def show
     # set_my_plant
   end
