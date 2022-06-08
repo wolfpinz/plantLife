5b9c3ccfcc404310f58a52774dd168b2f5e4e604
danielschuetzle <danielschuetzle@gmail.com>
2022-06-08 17:46:23 +0200

    garden card some style added with Css

--- db/seeds.rb
+++ db/seeds.rb
@@ -92,13 +92,24 @@ plants_array.each do |plants_hash|
   end
 
   Plant.create!(plants_hash)
-  
+
 end
 
 3.times do
   Garden.create(name: garden_names.sample, user: User.last)
   5.times do
-    MyPlant.create(garden: Garden.last, plant: Plant.all.sample, nickname: Faker::Name.first_name, last_watered: rand((DateTime.now - 2.weeks)..DateTime.now))
+    file = URI.open("https://cdn.shopify.com/s/files/1/0591/2746/4141/products/857MonsteraRiesemitSTab-min.jpg")
+    plant = MyPlant.create(garden: Garden.last,
+      plant: Plant.all.sample,
+      nickname: Faker::Name.first_name,
+      last_watered: rand((DateTime.now - 2.weeks)..DateTime.now)
+    )
+    plant.photo.attach(
+      io: file,
+      filename: "#{plant[:nickname]}",
+      content_type: 'image/png'
+    )
+    plant.save!
   end
 end
 
